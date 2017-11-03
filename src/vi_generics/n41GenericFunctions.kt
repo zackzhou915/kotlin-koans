9f0c7c96e8c51d9e03164e22c84f0fb8f12ddda3
zackzhou915 <574437725@qq.com>
2017-11-03 18:38:22 +0800

    interesting

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -2,9 +2,10 @@ package vi_generics
 
 import util.TODO
 import java.util.*
+import kotlin.collections.HashSet
 
 fun task41(): Nothing = TODO(
-    """
+        """
         Task41.
         Add a 'partitionTo' function that splits a collection into two collections according to a predicate.
         Uncomment the commented invocations of 'partitionTo' below and make them compile.
@@ -19,12 +20,26 @@ fun task41(): Nothing = TODO(
         }
 )
 
+// FIXME
+
+//inline fun <T> List<T>.partitionTo(_key: List<T>, _val: List<T>, predicate: (T) -> Boolean): Pair<List<T>, List<T>> {
+//    return partition(predicate)
+//}
+//
+//inline fun <T> Set<T>.partitionTo(_key: Set<T>, _val: Set<T>, predicate: (T) -> Boolean): Pair<Set<T>, Set<T>> {
+//    return with(partition(predicate)) {
+//        Pair(first.toCollection(HashSet<T>()), second.toCollection(HashSet<T>()))
+//    }
+//}
+
+inline fun <reified T, C : MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, predicate: (T) -> Boolean): Pair<C, C> {
+
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
 }
