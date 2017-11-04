1fa3f8d7d4a714f5a0b27285ee99264c61d775b6
zackzhou915 <574437725@qq.com>
2017-11-04 19:27:50 +0800

    vi_generics 41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -20,20 +20,15 @@ fun task41(): Nothing = TODO(
         }
 )
 
-// FIXME
-
-//inline fun <T> List<T>.partitionTo(_key: List<T>, _val: List<T>, predicate: (T) -> Boolean): Pair<List<T>, List<T>> {
-//    return partition(predicate)
-//}
-//
-//inline fun <T> Set<T>.partitionTo(_key: Set<T>, _val: Set<T>, predicate: (T) -> Boolean): Pair<Set<T>, Set<T>> {
-//    return with(partition(predicate)) {
-//        Pair(first.toCollection(HashSet<T>()), second.toCollection(HashSet<T>()))
-//    }
-//}
-
-inline fun <reified T, C : MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, predicate: (T) -> Boolean): Pair<C, C> {
-
+inline fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(a: C, b: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (ele in this) {
+        if (predicate(ele)) {
+            a.add(ele)
+        } else {
+            b.add(ele)
+        }
+    }
+    return Pair(a, b)
 }
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
